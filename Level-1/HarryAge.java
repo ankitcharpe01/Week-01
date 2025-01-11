af80d835f36c5442b0322a1fa14c03dba217320e
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-01-11 15:02:56 +0530

    Added Successfully

---
+++ Level-1/HarryAge.java
@@ -0,0 +1,8 @@
+
+public class HarryAge{
+     public static void main(String []args){
+		int birthYear =2000;
+		int currentYear =2024;
+		System.out.println("Harry's Age is : "+(currentYear-birthYear));
+	 }
+}
