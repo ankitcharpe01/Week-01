5393a803e14456f833aa1f7271ca4a6c684a35d7
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-01-11 18:35:35 +0530

    Added Successfully

---
+++ Level-2/FizzBuzz.java
@@ -0,0 +1,38 @@
+//import Scanner class
+import java.util.Scanner;
+
+public class FizzBuzz{
+	//main function
+    public static void main(String[] args) {
+	    //Creating a scanner class object
+        Scanner input = new Scanner(System.in);
+		
+         // Take a number as input 
+		System.out.println("Enter number: ");
+		int number = input.nextInt();
+		 
+		//check the number is positive integer
+		if(number>0){
+		for(int i=1 ;i<=number; i++){
+		
+		//print FizzBuzz if the number is multiple of 3  and 5 
+		  if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                }
+		//print Fizz if the number is multiple of 3	
+				else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                }
+		//print Buzz if the number is multiple of 5 		
+				else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+		 }
+        } else {
+            System.out.println("Is not a positive integer.");
+	}
+	input.close();
+}
+}
