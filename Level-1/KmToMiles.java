d299baa79baa93ac5602598ed6382ea9c0da4a84
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-01-11 15:03:50 +0530

    Added Successfully

---
+++ Level-1/KmToMiles.java
@@ -0,0 +1,11 @@
+public class KmToMiles{
+
+     public static void main(String []args){
+		//creating variable named kilometers and assign value
+		double kilometers =10.8;
+		//converting the distance of 10.8 kilometers to miles
+		double miles =kilometers*1.6;
+		//print the distance
+		System.out.println("The distance of 10.8 kilometers to miles is: "+ miles);
+	 }
+}
