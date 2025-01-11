bbc0592a29c0e816fa4e7cb84b33caca7df3af9a
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-01-11 15:00:46 +0530

    Added Successfully

---
+++ Level-1/AvgPercentMark.java
@@ -0,0 +1,11 @@
+public class AvgPercentMark{
+	public static void main(String []args){
+        //declaring variables
+		int maths =94;
+		int physics = 95;
+		int chemistry =96;
+		//printing average percent mark in PCM
+		System.out.println("Average percent mark in PCM is: "+(maths+physics+chemistry)/3);
+	}
+}
+ 
