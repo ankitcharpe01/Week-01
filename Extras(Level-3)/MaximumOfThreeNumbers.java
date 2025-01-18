125aa31be98163b6a671d8678264ac9c1e74ed72
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-01-18 10:54:20 +0530

    Added Succesfully

---
+++ Extras(Level-3)/MaximumOfThreeNumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MaximumOfThreeNumbers {
+    public static void main(String[] args) {
+        int num1 = getInput("Enter first number: ");
+        int num2 = getInput("Enter second number: ");
+        int num3 = getInput("Enter third number: ");
+
+        int max = findMax(num1, num2, num3);
+        System.out.println("The maximum number is: " + max);
+    }
+
+    public static int getInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(prompt);
+        return scanner.nextInt();
+    }
+
+    public static int findMax(int num1, int num2, int num3) {
+        int max = num1;
+        if (num2 > max) max = num2;
+        if (num3 > max) max = num3;
+        return max;
+    }
+}
