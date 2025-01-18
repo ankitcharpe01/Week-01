415c2a7f0d712840fc718572b98b01cd9d67bdcc
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-01-18 10:49:40 +0530

    Added Succesfully

---
+++ Extras(Level-1)/ReverseAString.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ReverseAString {
+    public static String stringReverse(String str){
+        String reverse = "";
+        for(int i=str.length()-1 ;i>=0;i--) {
+            reverse += str.charAt(i);
+        }
+        return reverse;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String str =sc.nextLine();
+        String reversedString =stringReverse(str);
+        System.out.println("The Reversed String Is: "+reversedString);
+    }
+}
