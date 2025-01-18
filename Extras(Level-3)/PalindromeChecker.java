8c28955e9e2f8847f66715ce0afd8cf5c2088ed7
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-01-18 10:55:15 +0530

    Added Succesfully

---
+++ Extras(Level-3)/PalindromeChecker.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string to check if it's a palindrome: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        String cleanedStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reversedStr = new StringBuilder(cleanedStr).reverse().toString();
+        return cleanedStr.equals(reversedStr);
+    }
+}
